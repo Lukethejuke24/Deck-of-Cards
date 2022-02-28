4761dc96be8f61958c0a37e7270da49087ca6401
ShadySharks <lucashamrick24@gmail.com>
2022-02-27 20:25:46 -0500

    Update

--- app.js
+++ app.js
@@ -7,14 +7,27 @@ const btnHit = document.getElementById("hit");
 const btnStand = document.getElementById("stand");
 const deckCount = 6;
 const shuffleUrl = "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count="+deckCount;
-var request = new XMLHttpRequest()
 let dealerDeck = [];
 let playerDeck = [];
 let deckId = "";
+let DealerScore = 0;
+let playerScore = 0;
 
 //Functions
-async function getData(){
-
+function getScore(cards){
+    let hasAce = false;
+    var score = cards.reduce((amt, card) => {
+      if (card.value === "ACE") {
+        hasAce = true;
+        return amt + 1
+      }
+      if (isNaN(card.value)) { return amt + 10 }
+      return amt + Number(card.value);
+    }, 0)
+    if (hasAce) {
+      score = (score + 10) > 21 ? score : score + 10;
+    }
+    return score
 }
 
 function reset(){
@@ -46,27 +59,32 @@ async function dealCards(deckId,amount){
 }
 
 function Hit(){
-    alert('You recieved a hit')
+    //alert('You recieved a hit')
     reset() ///reset everyting
 }
 
 function Deal(){
-    
+    //alert('Cards Dealt');
     fetch("https://www.deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=4")
     .then(dealtDeck => dealtDeck.json())
     .then(dealtDeck => {
         dealerDeck.push(dealtDeck[0],dealtDeck[2]);
         playerDeck.push(dealtDeck.cards[1],dealtDeck.cards[3])
         console.log(playerDeck)
-        alert('Cards Dealt');
-        
+        playerScore = getScore(playerDeck);
+        if (playerScore === 21) {
+          alert("BlackJack! You Win!") 
+        }
+        console.log(playerScore)
     })
+    
        
 }   
 
 
 function Stand(){
-    alert('You chose to stand')
+    ///alert('You chose to stand')
+    reset() ///reset everyting
 }
 
 
